b6f426f4bc3c8756316204b72fa464280b3fa40d
Fernando Reyes <fernando.reyes@du.edu>
2025-08-13 01:35:41 -0600

    added temporary message for kaltura processing

--- public/app/astools.module.js
+++ public/app/astools.module.js
@@ -178,7 +178,8 @@ const astoolsModule = (function () {
 
             // TODO
             if (is_kaltura === 'true') {
-
+                domModule.html('#message', '<div class="alert alert-danger"><i class=""></i> Kaltura processing temporarily down</div>');
+                return false;
                 domModule.html('#message', '<div class="alert alert-info"><i class=""></i> (' + folder + ') Retrieving Entry IDs from Kaltura...</div>');
 
                 let  result = await get_entry_ids(json);
